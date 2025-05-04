3975e3a63b0cbab51837f07eda92ec2658b16e37
Raoul Fuerst <raoul.fuerst@web.de>
2025-05-04 13:34:31 +0200

    Added saving of logs

--- app.py
+++ app.py
@@ -7,11 +7,24 @@ from analysis.performance_analyzer import PerformanceAnalyzer
 import json
 import logging
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+log_dir = 'logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+log_file = os.path.join(log_dir, f'backtesting_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler()
+    ]
+)
 logger = logging.getLogger(__name__)
 
 def replace_nan_with_none(obj):
